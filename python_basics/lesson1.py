333b938406f9cf1c9f35b65a3eff7983d6c32b7e
Leyva <oleyva@cps.k12.il.us>
2024-09-24 14:40:38 -0500

    first commit

---
+++ python_basics/lesson1.py
@@ -0,0 +1,26 @@
+#mkd means make directory
+#cd means change directory
+#ls makes list directory
+#pwd means print working directory
+#touch means create a file
+#rm means remove a file
+# pound sign means comment
+# print ("Hello, World!")
+# print("It's me, Oscar.")
+# print("My birthday is October \t 25 30th.")
+# # \t means tab
+# press control ? to comment out mutiple lines
+
+# ask for input
+#ask for age
+#print out age
+#ask for their place of birth 
+#ask for their address
+#ask what school they went to 
+#print out school
+
+
+
+
+name = input("What is your name?" + )
+print("Hello, " + name + )
